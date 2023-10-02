84b33444099119c62ced1467dd831d25aea62e0d
Auto <Auto@test.com>
2023-10-02 06:06:23 +0000

    [Auto] Update online example

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -21,9 +21,9 @@ const RESOURCES = {"canvaskit/chromium/canvaskit.js": "96ae916cd2d1b7320fff853ee
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "a6b3ff9941963a1fde37c08224c2c7d9",
-"/": "a6b3ff9941963a1fde37c08224c2c7d9",
-"main.dart.js": "207876a377b8b158da0f6c5b5fad32b4",
+"index.html": "f99ecbd862e99e4ed35d25d4eb4829e2",
+"/": "f99ecbd862e99e4ed35d25d4eb4829e2",
+"main.dart.js": "69b4a008b7b170275145c71c0d1b5e17",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "manifest.json": "0867c3e13649ac4d06fe34b7b3ddce08"};
