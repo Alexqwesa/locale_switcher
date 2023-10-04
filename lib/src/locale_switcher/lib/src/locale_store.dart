355d23570b00d4582da4517c437fbf55b503957b
Aleksandr Savin <alexo.veto@gmail.com>
2023-10-04 10:49:20 +0700

    StringToLocaleHelper

--- lib/src/locale_switcher/lib/src/locale_store.dart
+++ lib/src/locale_switcher/lib/src/locale_store.dart
@@ -251,3 +251,27 @@ class _LocaleObserver extends WidgetsBindingObserver {
     onChanged(locales);
   }
 }
+
+extension StringToLocaleHelper on String {
+  /// Convert string to [Locale] object
+  Locale toLocale({String separator = '_'}) {
+    final localeList = split(separator);
+    switch (localeList.length) {
+      case 2:
+        return localeList.last.length == 4 // scriptCode length is 4
+            ? Locale.fromSubtags(
+                languageCode: localeList.first,
+                scriptCode: localeList.last,
+              )
+            : Locale(localeList.first, localeList.last);
+      case 3:
+        return Locale.fromSubtags(
+          languageCode: localeList.first,
+          scriptCode: localeList[1],
+          countryCode: localeList.last,
+        );
+      default:
+        return Locale(localeList.first);
+    }
+  }
+}
