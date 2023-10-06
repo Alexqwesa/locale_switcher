94ee8d5c53be52036c1f1669d15f12d586f6df06
Aleksandr Savin <alexo.veto@gmail.com>
2023-10-06 09:43:08 +0700

    delete packBuilder - will only exist in locale_switcher

--- lib/src/builders/builders.dart
+++ lib/src/builders/builders.dart
@@ -1,13 +1,10 @@
 library builder;
 
 import 'package:build/build.dart';
-import 'package:locale_switcher_dev/src/builders/pack/pack_builder.dart';
 import 'package:locale_switcher_dev/src/builders/package_builder.dart';
 
 Builder packageBuilder([BuilderOptions? options]) {
   return PackageBuilder();
 }
 
-Builder packBuilder([BuilderOptions? options]) {
-  return PackBuilder();
-}
+
