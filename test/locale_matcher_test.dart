1dc6e8a33bdb2be97daf11080a64cf80af6d8252
Aleksandr Savin <alexo.veto@gmail.com>
2023-10-28 12:31:25 +0700

    new tests

--- test/locale_matcher_test.dart
+++ test/locale_matcher_test.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:locale_switcher/locale_switcher.dart';
 
@@ -42,5 +44,112 @@ void main() {
 
     expect(LocaleMatcher.tryFindLocale('vi')?.locale?.countryCode, 'vn');
     expect(LocaleMatcher.byLanguage('vi')?.locale?.countryCode, 'vn');
+  });
+
+  test('tryFindLocale returns a valid LocaleName when found by name', () {
+    final names = <String>['en_gb', 'vi_vn', 'de_de'];
+    LocaleSwitcher.readLocales(names.map((String e) => e.toLocale()).toList());
+    final result = LocaleMatcher.tryFindLocale('en_US');
+    expect(result, isNotNull);
+    expect(result!.name, 'en_gb');
+
+    final result2 = LocaleMatcher.tryFindLocale('en_gb');
+    expect(result2, isNotNull);
+    expect(result2!.name, 'en_gb');
+  });
+
+  test('tryFindLocale returns a valid LocaleName when found by language', () {
+    final names = <String>['en_gb', 'vi_vn', 'de_de'];
+    LocaleSwitcher.readLocales(names.map((String e) => e.toLocale()).toList());
+    final result = LocaleMatcher.tryFindLocale('en');
+    expect(result, isNotNull);
+    expect(result!.name, 'en_gb'); // Assuming 'en' maps to 'en_US' in your code
+  });
+
+  test(
+      'tryFindLocale returns null when not found and IfLocaleNotFound is doNothing',
+      () {
+    final result = LocaleMatcher.tryFindLocale('non_existent_locale');
+    expect(result, isNull);
+  });
+
+  test(
+      'tryFindLocale returns a valid LocaleName when IfLocaleNotFound is useSystem ',
+      () {
+    final names = <String>['vi_vn', 'de_de', 'en_gb'];
+    LocaleSwitcher.readLocales(names.map((String e) => e.toLocale()).toList());
+
+    final result = LocaleMatcher.tryFindLocale('non_existent_locale',
+        ifLocaleNotFound: IfLocaleNotFound.useSystem);
+    expect(result, isNotNull);
+    expect(result!.name, 'system');
+    expect(result.bestMatch.toString(), 'en_gb');
+    expect(result.locale.toString(), 'en_US');
+  });
+
+  test(
+      'tryFindLocale returns system when IfLocaleNotFound is useSystem and system locale is not found',
+      () {
+    final names = <String>['en_gb', 'vi_vn', 'de_de'];
+    LocaleSwitcher.readLocales(names.map((String e) => e.toLocale()).toList());
+
+    final result = LocaleMatcher.tryFindLocale('non_existent_locale',
+        ifLocaleNotFound: IfLocaleNotFound.useSystem);
+    expect(result!.name, 'system');
+  });
+
+  test(
+      'tryFindLocale returns a valid LocaleName when IfLocaleNotFound is useFirst and a valid locale is not found',
+      () {
+    final names = <String>['de_de', 'en_gb', 'vi_vn'];
+    LocaleSwitcher.readLocales(names.map((String e) => e.toLocale()).toList());
+
+    final result = LocaleMatcher.tryFindLocale('non_existent_locale',
+        ifLocaleNotFound: IfLocaleNotFound.useFirst);
+    expect(result, isNotNull);
+    expect(result!.name, 'de_de');
+  });
+
+  test('byLocale returns a valid LocaleName ', () {
+    final names = <String>['de_de', 'en_gb', 'vi_vn'];
+    LocaleSwitcher.readLocales(names.map((String e) => e.toLocale()).toList());
+
+    final result =
+        LocaleMatcher.byLocale(LocaleSwitcher.supportedLocaleNames[3].locale!);
+    expect(result, isNotNull);
+    expect(result!.name, 'vi_vn');
+
+    // exact match
+    final result2 = LocaleMatcher.byLocale(const Locale('vi'));
+    expect(result2, isNull);
+  });
+
+  test('LocaleName toString() ', () {
+    final names = <String>['de_de', 'en_gb', 'vi_vn'];
+    LocaleSwitcher.readLocales(names.map((String e) => e.toLocale()).toList());
+
+    expect(LocaleSwitcher.supportedLocaleNames.firstOrNull, isNotNull);
+    expect(LocaleSwitcher.supportedLocaleNames.firstOrNull!.toString(),
+        "system|en");
+  });
+
+  test('LocaleName showOtherLocales failsafe ', () {
+    final names = <String>['de_de', 'en_gb', 'vi_vn'];
+    final lnf =
+        SupportedLocaleNames(names.map((String e) => e.toLocale()).toList());
+    lnf.addShowOtherLocales();
+
+    expect(lnf[4], isNotNull);
+    expect(lnf[4].bestMatch.toString(), "de_de");
+  });
+
+  test('LocaleName showOtherLocales failsafe ', () {
+    final names = <String>['de_de', 'en_gb', 'vi_vn'];
+    final lnf = SupportedLocaleNames(
+        names.map((String e) => e.toLocale()).toList(),
+        showOsLocale: true);
+    lnf.replaceLast(str: 'de');
+    expect(lnf[0].toString(), "system|en");
+    expect(lnf.last.toString(), "vi_vn|vi");
   });
 }
